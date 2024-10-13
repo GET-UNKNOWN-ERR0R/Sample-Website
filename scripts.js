1222fc0cc3502b9e8c798f7dc03a58daa69a75a2
ARPIT PRAJAPATI <getunknownerror@gmail.com>
2024-10-13 14:19:51 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 document.querySelector('.load-more').addEventListener('click', function () {
     alert('Load more posts...');
-    // Here you would typically make an AJAX request to load more posts
+    
 });
